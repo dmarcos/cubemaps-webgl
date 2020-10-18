c2d4c9a07160869ab403501c579dcfb0c7fdd02d
Diego Marcos <diego.marcos@gmail.com>
2020-10-18 11:20:02 -0700

    add layer example

---
+++ cubemap-compressed/main.js
@@ -0,0 +1,360 @@
+// WebGL - Textures - Cubemap
+// from https://webglfundamentals.org/webgl/webgl-cubemap.html
+
+"use strict";
+
+function main() {
+  // Get A WebGL context
+  /** @type {HTMLCanvasElement} */
+  var canvas = document.querySelector("#canvas");
+  var gl = canvas.getContext("webgl");
+  if (!gl) {
+    return;
+  }
+
+  // setup GLSL program
+  var program = createProgramFromScripts(gl, ["vertex-shader-3d", "fragment-shader-3d"]);
+
+  // look up where the vertex data needs to go.
+  var positionLocation = gl.getAttribLocation(program, "a_position");
+
+  // lookup uniforms
+  var matrixLocation = gl.getUniformLocation(program, "u_matrix");
+  var textureLocation = gl.getUniformLocation(program, "u_texture");
+
+  // Create a buffer for positions
+  var positionBuffer = gl.createBuffer();
+  // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  // Put the positions in the buffer
+  setGeometry(gl);
+
+  function radToDeg(r) {
+    return r * 180 / Math.PI;
+  }
+
+  function degToRad(d) {
+    return d * Math.PI / 180;
+  }
+
+  var fieldOfViewRadians = degToRad(60);
+  var modelXRotationRadians = degToRad(0);
+  var modelYRotationRadians = degToRad(0);
+
+  // Get the starting time.
+  var then = 0;
+
+  // Create a texture.
+  var texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_CUBE_MAP, texture);
+
+  loadBasisTexture(texture, function () {
+    requestAnimationFrame(drawScene);
+  });
+
+  const ctx = document.createElement("canvas").getContext("2d");
+
+  ctx.canvas.width = 512;
+  ctx.canvas.height = 512;
+
+  gl.generateMipmap(gl.TEXTURE_CUBE_MAP);
+
+  gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+  gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+
+  // Draw the scene.
+  function drawScene(time) {
+    // convert to seconds
+    time *= 0.001;
+    // Subtract the previous time from the current time
+    var deltaTime = time - then;
+    // Remember the current time for the next frame.
+    then = time;
+
+    resizeCanvasToDisplaySize(gl.canvas);
+
+    // Tell WebGL how to convert from clip space to pixels
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    gl.enable(gl.CULL_FACE);
+    gl.enable(gl.DEPTH_TEST);
+
+    // Animate the rotation
+    modelYRotationRadians += -0.7 * deltaTime;
+    modelXRotationRadians += -0.4 * deltaTime;
+
+    // Clear the canvas AND the depth buffer.
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    // Tell it to use our program (pair of shaders)
+    gl.useProgram(program);
+
+    // Turn on the position attribute
+    gl.enableVertexAttribArray(positionLocation);
+
+    // Bind the position buffer.
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    // Tell the position attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+    var size = 3;          // 3 components per iteration
+    var type = gl.FLOAT;   // the data is 32bit floats
+    var normalize = false; // don't normalize the data
+    var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+    var offset = 0;        // start at the beginning of the buffer
+    gl.vertexAttribPointer(
+        positionLocation, size, type, normalize, stride, offset);
+
+    // Compute the projection matrix
+    var aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    var projectionMatrix =
+        m4.perspective(fieldOfViewRadians, aspect, 1, 2000);
+
+    var cameraPosition = [0, 0, 2];
+    var up = [0, 1, 0];
+    var target = [0, 0, 0];
+
+    // Compute the camera's matrix using look at.
+    var cameraMatrix = m4.lookAt(cameraPosition, target, up);
+
+    // Make a view matrix from the camera matrix.
+    var viewMatrix = m4.inverse(cameraMatrix);
+
+    var viewProjectionMatrix = m4.multiply(projectionMatrix, viewMatrix);
+
+    var matrix = m4.xRotate(viewProjectionMatrix, modelXRotationRadians);
+    matrix = m4.yRotate(matrix, modelYRotationRadians);
+
+    // Set the matrix.
+    gl.uniformMatrix4fv(matrixLocation, false, matrix);
+
+    // Tell the shader to use texture unit 0 for u_texture
+    gl.uniform1i(textureLocation, 0);
+
+    // Draw the geometry.
+    gl.drawArrays(gl.TRIANGLES, 0, 6 * 6);
+
+    requestAnimationFrame(drawScene);
+  }
+
+  function loadBasisTexture(src, callback) {
+    var loader = new THREE.BasisTextureLoader();
+    var self = this;
+    var extension = gl.getExtension('EXT_texture_compression_bptc');
+    loader.setTranscoderPath( './' );
+    loader.detectSupport( gl );
+    loader.load('../bricks.basis', function ( evt ) {
+      debugger;
+      var buffer = evt.mipmaps[0];
+      var internalFormat = extension.COMPRESSED_RGBA_BPTC_UNORM_EXT;
+      var width = buffer.width;
+      var height = buffer.height;
+      const level = 0;
+
+      gl.compressedTexImage2D(
+        gl.TEXTURE_CUBE_MAP_POSITIVE_X, level,
+        internalFormat,
+        width, height, // width, height of the image
+        0, // border, always 0
+        buffer.data);
+
+      gl.compressedTexImage2D(
+        gl.TEXTURE_CUBE_MAP_NEGATIVE_X, level,
+        internalFormat,
+        width, height, // width, height of the image
+        0, // border, always 0
+        buffer.data);
+
+      gl.compressedTexImage2D(
+        gl.TEXTURE_CUBE_MAP_POSITIVE_Y, level,
+        internalFormat,
+        width, height, // width, height of the image
+        0, // border, always 0
+        buffer.data);
+
+      gl.compressedTexImage2D(
+        gl.TEXTURE_CUBE_MAP_NEGATIVE_Y, level,
+        internalFormat,
+        width, height, // width, height of the image
+        0, // border, always 0
+        buffer.data);
+
+      gl.compressedTexImage2D(
+        gl.TEXTURE_CUBE_MAP_POSITIVE_Z, level,
+        internalFormat,
+        width, height, // width, height of the image
+        0, // border, always 0
+        buffer.data);
+
+      gl.compressedTexImage2D(
+        gl.TEXTURE_CUBE_MAP_NEGATIVE_Z, level,
+        internalFormat,
+        width, height, // width, height of the image
+        0, // border, always 0
+        buffer.data);
+
+      let errorCode = gl.getError();
+      if (errorCode !== 0) {
+        console.log('renderingError, WebGL Error Code: ' + errorCode);
+      }
+
+      if(callback) { callback(texture); }
+
+    }, undefined, function ( error ) {
+      console.error( error );
+    });
+  }
+}
+
+// Fill the buffer with the values that define a cube.
+function setGeometry(gl) {
+  var positions = new Float32Array(
+    [
+    -0.5, -0.5,  -0.5,
+    -0.5,  0.5,  -0.5,
+     0.5, -0.5,  -0.5,
+    -0.5,  0.5,  -0.5,
+     0.5,  0.5,  -0.5,
+     0.5, -0.5,  -0.5,
+
+    -0.5, -0.5,   0.5,
+     0.5, -0.5,   0.5,
+    -0.5,  0.5,   0.5,
+    -0.5,  0.5,   0.5,
+     0.5, -0.5,   0.5,
+     0.5,  0.5,   0.5,
+
+    -0.5,   0.5, -0.5,
+    -0.5,   0.5,  0.5,
+     0.5,   0.5, -0.5,
+    -0.5,   0.5,  0.5,
+     0.5,   0.5,  0.5,
+     0.5,   0.5, -0.5,
+
+    -0.5,  -0.5, -0.5,
+     0.5,  -0.5, -0.5,
+    -0.5,  -0.5,  0.5,
+    -0.5,  -0.5,  0.5,
+     0.5,  -0.5, -0.5,
+     0.5,  -0.5,  0.5,
+
+    -0.5,  -0.5, -0.5,
+    -0.5,  -0.5,  0.5,
+    -0.5,   0.5, -0.5,
+    -0.5,  -0.5,  0.5,
+    -0.5,   0.5,  0.5,
+    -0.5,   0.5, -0.5,
+
+     0.5,  -0.5, -0.5,
+     0.5,   0.5, -0.5,
+     0.5,  -0.5,  0.5,
+     0.5,  -0.5,  0.5,
+     0.5,   0.5, -0.5,
+     0.5,   0.5,  0.5,
+
+    ]);
+  gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
+}
+
+const defaultShaderType = [
+  'VERTEX_SHADER',
+  'FRAGMENT_SHADER',
+];
+
+function createProgramFromScripts(gl, shaderScriptIds, opt_attribs, opt_locations, opt_errorCallback) {
+  const shaders = [];
+  for (let ii = 0; ii < shaderScriptIds.length; ++ii) {
+    shaders.push(createShaderFromScript(gl, shaderScriptIds[ii], gl[defaultShaderType[ii]], opt_errorCallback));
+  }
+  return createProgram(gl, shaders, opt_attribs, opt_locations, opt_errorCallback);
+}
+
+function createShaderFromScript(gl, scriptId, opt_shaderType, opt_errorCallback) {
+  let shaderSource = '';
+  let shaderType;
+  const shaderScript = document.getElementById(scriptId);
+  if (!shaderScript) {
+    throw ('*** Error: unknown script element' + scriptId);
+  }
+  shaderSource = shaderScript.text;
+
+  if (!opt_shaderType) {
+    if (shaderScript.type === 'x-shader/x-vertex') {
+      shaderType = gl.VERTEX_SHADER;
+    } else if (shaderScript.type === 'x-shader/x-fragment') {
+      shaderType = gl.FRAGMENT_SHADER;
+    } else if (shaderType !== gl.VERTEX_SHADER && shaderType !== gl.FRAGMENT_SHADER) {
+      throw ('*** Error: unknown shader type');
+    }
+  }
+
+  return loadShader(
+      gl, shaderSource, opt_shaderType ? opt_shaderType : shaderType,
+      opt_errorCallback);
+}
+
+function loadShader(gl, shaderSource, shaderType, opt_errorCallback) {
+  const errFn = opt_errorCallback || function () {};
+  // Create the shader object
+  const shader = gl.createShader(shaderType);
+
+  // Load the shader source
+  gl.shaderSource(shader, shaderSource);
+
+  // Compile the shader
+  gl.compileShader(shader);
+
+  // Check the compile status
+  const compiled = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (!compiled) {
+    // Something went wrong during compilation; get the error
+    const lastError = gl.getShaderInfoLog(shader);
+    errFn('*** Error compiling shader \'' + shader + '\':' + lastError);
+    gl.deleteShader(shader);
+    return null;
+  }
+
+  return shader;
+}
+
+function createProgram(gl, shaders, opt_attribs, opt_locations, opt_errorCallback) {
+  const errFn = opt_errorCallback || function () {};
+  const program = gl.createProgram();
+  shaders.forEach(function(shader) {
+    gl.attachShader(program, shader);
+  });
+  if (opt_attribs) {
+    opt_attribs.forEach(function(attrib, ndx) {
+      gl.bindAttribLocation(
+          program,
+          opt_locations ? opt_locations[ndx] : ndx,
+          attrib);
+    });
+  }
+  gl.linkProgram(program);
+
+  // Check the link status
+  const linked = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (!linked) {
+      // something went wrong with the link
+      const lastError = gl.getProgramInfoLog(program);
+      errFn('Error in program linking:' + lastError);
+
+      gl.deleteProgram(program);
+      return null;
+  }
+  return program;
+}
+
+function resizeCanvasToDisplaySize(canvas, multiplier) {
+  multiplier = multiplier || 1;
+  const width  = canvas.clientWidth  * multiplier | 0;
+  const height = canvas.clientHeight * multiplier | 0;
+  if (canvas.width !== width ||  canvas.height !== height) {
+    canvas.width  = width;
+    canvas.height = height;
+    return true;
+  }
+  return false;
+}
+
+main();
